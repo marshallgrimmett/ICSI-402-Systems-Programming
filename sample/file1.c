b33aeb2aa0a264e5f199cfff2099ab01c6f89306
Marshall Grimmett <marshallmg6@gmail.com>
2018-09-11 13:54:59 -0400

    Created Sample

---
+++ sample/file1.c
@@ -0,0 +1,9 @@
+#include "externs.h"
+
+float f1(int a) {
+  return (float)a;
+}
+
+int f2(char a) {
+  return ((p + q + a) - ('a' + 'A'));
+}
---
+++ sample/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "globals.h"
+
+int g1 = 0;
+int g2 = 0;
+
+int main() {
+  float f1(int);
+  int f2(char);
+
+  int i = 5;
+  char c ='x';
+
+  printf("%f\n", f1(i + p + q));
+  printf("%d\n", f2(c));
+
+  return 0;
+}
