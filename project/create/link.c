60e78f4062032910c8c239864c5fac00665cc1dd
Marshall Grimmett <marshallmg6@gmail.com>
2018-09-25 11:53:26 -0400

    Renamed hardline and updated

---
+++ project/create/link.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h> // link, symlink
+#include <errno.h>
+
+void createLink(char *oldFile, char *newFile) {
+  int fd;
+  
+  if ((fd = link(oldFile, newFile)) != -1) {
+    close(fd);
+  }
+  else {
+    printf("error creating link. error: %d\n", errno);
+  }
+}
