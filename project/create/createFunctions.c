5c85f63c9966fa2826387a5f5641d69c479b191f
Marshall Grimmett <marshallmg6@gmail.com>
2018-09-25 11:51:23 -0400

    Updated createFunctions.c

--- project/create/createFunctions.c
+++ project/create/createFunctions.c
@@ -1,19 +1,21 @@
 #include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 
 #define NUM_FD_ARGS 2
 #define NUM_HS_ARGS 3
+
+void createLink(char *oldFile, char *newFile);
+void createSymLink(char *oldFile, char *newFile);
 
 void create(int argc, char *argv[]) {
   if (argc == (NUM_FD_ARGS + 1)) {
     if (strcmp(argv[1], "-f") == 0) {
       // create file
-      printf("not finished.\n");
+      // TODO
     }
     else if (strcmp(argv[1], "-d") == 0) {
       // create directory
-      printf("not finished.\n");
+      // TODO
     }
     else {
       printf("invalid command.\n");
@@ -22,11 +24,11 @@ void create(int argc, char *argv[]) {
   else if (argc == (NUM_HS_ARGS + 1)) {
     if (strcmp(argv[1], "-h") == 0) {
       // create hard link
-      link(argv[2], argv[3]);
+      createLink(argv[2], argv[3]);
     }
     else if (strcmp(argv[1], "-s") == 0) {
       // create soft link
-      symlink(argv[2], argv[3]);
+      createSymLink(argv[2], argv[3]);
     }
     else {
       printf("invalid command.\n");
