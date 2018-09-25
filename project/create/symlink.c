1c16cd20823570cef8613a9c18c33d6720c21072
Marshall Grimmett <marshallmg6@gmail.com>
2018-09-25 11:53:57 -0400

    Renamed softlink and updated

---
+++ project/create/symlink.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h> // link, symlink
+#include <errno.h>
+
+void createSymLink(char *oldFile, char *newFile) {
+  int fd;
+  
+  if ((fd = symlink(oldFile, newFile)) != -1) {
+    close(fd);
+  }
+  else {
+    printf("error creating link. error: %d\n", errno);
+  }
+}
