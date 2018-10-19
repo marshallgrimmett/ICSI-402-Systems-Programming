3bdb921857d0c2d63b0bd7d058bc6765f35994bf
Glen Sequeira <glensequeira8@gmail.com>
2018-10-18 20:19:27 -0400

    bubble sort function added for sortiing the records

--- project/fileConverter/sorting.c
+++ project/fileConverter/sorting.c
@@ -56,24 +56,43 @@ int compare(char *str1, char *str2){
     return 0;
 
 }
-void swap(char *xp, char *yp) 
-{ 
-    char *temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-}
+// void swap(char *xp, char *yp) 
+// { 
+//     char *temp;
+    
+    
+// }
 
-void bubbleSort(char arr[], const int n) 
+void bubbleSort(char **arr, const int n) 
 {
-    
-    
+    char record[50],record1[50];
+    char *temp,*temprecord;
+    char *date1,*date2;
    int i, j; 
-   for (i = 0; i < n-1; i++)       
-  
+   for (i = 0; i < n-1; i++){  
+
        // Last i elements are already in place    
-       for (j = 0; j < n-i-1; j++)  
-           if (compare(arr[j],arr[j+1])) 
-              swap(&arr[j], &arr[j+1]); 
+       for (j = 0; j < n-i-1; j++)  {
+        strcpy(record,arr[j]);
+        
+        temp= strtok(record," ");
+        strtok(NULL," ");
+        strtok(NULL," ");
+        date1=strtok(NULL,"");
+        strcpy(record1,arr[j+1]);
+        temp= strtok(record1," ");
+        strtok(NULL," ");
+        strtok(NULL," ");
+        date2=strtok(NULL,"");
+        //printf("date1:%s\tdate2:%s\there 1:%d\n",date1,date2,compare(date1,date2));
+           if (compare(date1,date2)==1){
+                strcpy(temprecord,arr[j]); 
+                strcpy(arr[j],arr[j+1]);
+                 //printf("here :%s",date2);
+                strcpy(arr[j+1],temprecord); 
+            }
+       } 
+}
 } 
 
 
@@ -85,18 +104,26 @@ void sort(){
 
 
 int main(){
-const char *flights[10];
-flights[0] = "AA43 DFW DTW 2016-01-06 11:00";
-flights[1] ="AA43 DFW DTW 2016-01-17 11:00";
-flights[2] ="AA44 SEA JFK 2015-02-05 7:45";
-flights[3] ="AA197 ORD BOS 2012-03-12 11:50";
-flights[4] ="AA197 ORD BOS 2012-03-12 11:50";
-flights[5] ="AA1 JFK LAX 2016-07-02 9:00";
-flights[6] ="OO7435 DTW PLN 2016-11-22 21:55";
-flights[7] ="F9612 DEN MIA 2014-12-19 22:15";
-flights[8] ="DL801 GEG MSP 2016-08-31 9:00";
-flights[9] ="DL1087 ATL DAB 2016-04-10 12:05";
-flights[10] ="DL828 IAH SLC 2012-06-02 7:45";
+char **flights;
+flights = (char **) malloc(10 * sizeof(char*));
+for (int j = 0; j < 10; j++) {
+    flights[j] = (char *) malloc(50 * sizeof(char*));
+}
+
+strcpy(flights[0],"AA43 DFW DTW 2016-01-06 11:00");
+strcpy(flights[1],"AA43 DFW DTW 2016-01-17 11:00");
+strcpy(flights[2],"AA44 SEA JFK 2015-02-05 7:45");
+strcpy(flights[3],"AA197 ORD BOS 2012-03-12 11:50");
+strcpy(flights[4],"AA197 ORD BOS 2012-03-12 11:50");
+strcpy(flights[5],"AA1 JFK LAX 2016-07-02 9:00");
+strcpy(flights[6],"OO7435 DTW PLN 2016-11-22 21:55");
+strcpy(flights[7],"F9612 DEN MIA 2014-12-19 22:15");
+strcpy(flights[8],"DL801 GEG MSP 2016-08-31 9:00");
+strcpy(flights[9],"DL1087 ATL DAB 2016-04-10 12:05");
+    // for(int i=0;i<10;i++){
+    //     printf("\n%s",flights[i]);
+    // }
+
     char str1[20]=" 2016-01-06 11:00";
     char str2[20]=" 2018-01-06 1:00";
     // char *temp= strtok(str1," ");
@@ -106,14 +133,19 @@ flights[10] ="DL828 IAH SLC 2012-06-02 7:45";
     //  char date1= strtok(NULL,"-");
     //  char hour1= strtok(time1,"-");
     //  char min1=strtok(NULL,"-");
-    char arr[50];
-    strcpy(arr,"AA43 DFW DTW 2016-01-06 11:00");
-    char *temp= strtok(arr," ");
-    printf("%s",temp);
-    strtok(NULL," ");
-    strtok(NULL," ");
-    char *date=strtok(NULL,"");
+    // char arr[50];
+    // strcpy(arr,"AA43 DFW DTW 2016-01-06 11:00");
+    // char *temp= strtok(arr," ");
+    // printf("%s",temp);
+    // strtok(NULL," ");
+    // strtok(NULL," ");
+    // char *date=strtok(NULL,"");
+    
     int val=compare("2016-01-06 1:00","2016-01-06 11:00");
-    printf("value is: %s",date);
+    bubbleSort(flights,10);
+    
+    for(int i=0;i<10;i++){
+        printf("\n%s",flights[i]);
+    }
 
 }
