cf0ffb7c9c3b6d8aabf09ac53ccd0281a6ba054d
Marshall Grimmett <marshallmg6@gmail.com>
2018-11-06 10:56:39 -0500

    Added pseudocode for srchindx

--- project/srchindxFunctions.c
+++ project/srchindxFunctions.c
@@ -2,4 +2,38 @@
 
 void srchindx(char *flag, char *terms, char *indexFileName, char *path) {
   // TODO
+
+  // Question: based on the flag, you only want hashtable OR BST, not both?
+
+  // PART 1
+  // if flag is -o, create a hashtable
+  // Construct hashtable
+
+  // PART 2
+  // if flag is -d, create a BST
+  // construct BST
+
+  // Construct BST or hashtable for each of the terms
+
+
+  // The next parts need work
+
+
+  // PART 3
+  // function to recursively search directory (and subdirectories) for files.
+
+  // PART 4
+  // for each term
+  //    find the term in the index file
+  //    search each record for the term(airport), based on the flag (-o search origin airports only)
+  //      if the term matches, add the record to the BST or hashtable
+  //    stop searching once the count is hit
+  // missing anything?
+
+  // PART 7
+  // Need function(s) for retrieving elements from strings.
+  // I.e. retrieve airport code from a record
+
+  // PART 6
+  // Print the contents of the BST or hashtable
 }
